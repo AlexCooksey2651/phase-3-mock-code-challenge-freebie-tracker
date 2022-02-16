b189117f3cf9c41c2c2cb71d16e6a2f69916ba88
Alex Cooksey <aecooksey2651@gmail.com>
2022-02-16 18:48:29 -0500

    Completed assignment

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,16 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one? (item_name)
+        !!self.freebies.find_by(item_name: item_name)
+    end
+
+    def give_away (other_dev, freebie)
+        if freebie.dev == self
+            freebie.update(dev: other_dev)
+        else
+            p "can't give away"
+        end
+    end
 end
